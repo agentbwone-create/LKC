02594dd0c750b70d45d7f8a806267a92b59fa0f8
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2026-03-03 04:14:51 +0600

    test debug true

--- lkc_school/settings.py
+++ lkc_school/settings.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-lkc-school-management-2026-change-in-production')
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = True
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 CSRF_TRUSTED_ORIGINS = [
     'https://*.railway.app',
