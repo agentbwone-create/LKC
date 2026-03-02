9ecba2100578c3d3cf35b7be99640d4932675667
agentbwone-create <agentbwone@gmail.com>
2026-03-02 20:12:26 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-gunicorn lkc_school.wsgi:application --bind 0.0.0.0:8000 --workers 2 --timeout 120
-```
-
-5. Commit the file
-
-### **Step 2: Update Your Procfile**
-
-Edit your `Procfile` and change it to:
-```
-web: bash start.sh
+exec gunicorn lkc_school.wsgi:application --bind 0.0.0.0:8000 --workers 2 --timeout 120
