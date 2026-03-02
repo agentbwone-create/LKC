30dcf6227a9c6a34ac1de1b2999f47fa111134c8
agentbwone-create <agentbwone@gmail.com>
2026-03-02 16:20:02 +0200

    Delete lkc_school_railway directory

--- lkc_school_railway/apps/attendance/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import AttendanceViewSet, AttendanceRegisterViewSet
-router = DefaultRouter()
-router.register('registers', AttendanceRegisterViewSet, basename='registers')
-router.register('', AttendanceViewSet, basename='attendance')
-urlpatterns = [path('', include(router.urls))]
