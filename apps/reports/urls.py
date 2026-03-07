6f9a4ce269029b76073851e1f95aefb7f6e4a606
agentbwone-create <agentbwone@gmail.com>
2026-03-07 10:09:37 +0200

    Update urls.py
    1 class teacher should see overall perfomance of students in their class across all subjects

--- apps/reports/urls.py
+++ apps/reports/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HODReportView, ReportPreviewView
+from .views import HODReportView, ReportPreviewView, ClassTeacherPerformanceView
 urlpatterns = [
     path('hod-report/', HODReportView.as_view(), name='hod-report'),
     path('hod-preview/', ReportPreviewView.as_view(), name='hod-preview'),
+    path('class-performance/', ClassTeacherPerformanceView.as_view(), name='class-performance'),
 ]
